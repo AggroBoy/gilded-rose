913d528deaeaef1d19d3fbe7f554343ec20ad77f
Will Goring <will@shadowrealm.org>
2019-01-28 20:36:13 +0000

    adding simple sulfuras test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,27 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void testSulfuras() {
+        Item[] items = new Item[] {
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Sulfuras, Hand of Ragnaros", 10, 80)
+        };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals(80, items[0].quality);
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[1].quality);
+        assertEquals(-1, items[1].sellIn);
+        assertEquals(80, items[2].quality);
+        assertEquals(10, items[2].sellIn);
     }
 
 }
