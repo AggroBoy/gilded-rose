18e89f66b52b729bac176079ded45e85cfb142d0
Will Goring <will@shadowrealm.org>
2019-01-28 21:16:03 +0000

    Added missing test cases
    quality is bounded (0,50) for all non Legendary items

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -32,7 +32,8 @@ public class GildedRoseTest {
         Item[] items = new Item[]{
                 new Item("Happy Fun Rock", 10, 20),
                 new Item("Heavy Leather Ball", 1, 5),
-                new Item("Voodoo Skull", 0, 10)
+                new Item("Voodoo Skull", 0, 10),
+                new Item("Paper Zeppelin Kit", 3, 0)
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -40,59 +41,73 @@ public class GildedRoseTest {
         assertItemEquals(items[0], 9, 19);
         assertItemEquals(items[1], 0, 4);
         assertItemEquals(items[2], -1, 8);
+        assertItemEquals(items[3], 2, 0);
 
         app.updateQuality();
         assertItemEquals(items[0], 8, 18);
         assertItemEquals(items[1], -1, 2);
         assertItemEquals(items[2], -2, 6);
+        assertItemEquals(items[3], 1, 0);
     }
 
     @Test
     public void testBrie() {
         Item[] items = new Item[]{
-                new Item("Aged Brie", 2, 10)
+                new Item("Aged Brie", 2, 10),
+                new Item("Aged Brie", 2, 49)
         };
 
         GildedRose app = new GildedRose(items);
 
         app.updateQuality();
         assertItemEquals(items[0], 1, 11);
+        assertItemEquals(items[1], 1, 50);
 
 
         app.updateQuality();
         assertItemEquals(items[0], 0, 12);
+        assertItemEquals(items[1], 0, 50);
 
         app.updateQuality();
         assertItemEquals(items[0], -1, 14);
+        assertItemEquals(items[1], -1, 50);
     }
 
     @Test
     public void testChieftains() {
         Item[] items = new Item[]{
-                new Item("Backstage passes to a TAFKAL80ETC concert", 12, 10)
+                new Item("Backstage passes to a TAFKAL80ETC concert", 12, 10),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 12, 50)
         };
 
         GildedRose app = new GildedRose(items);
 
         app.updateQuality();
         assertItemEquals(items[0], 11, 11);
+        assertItemEquals(items[1], 11, 50);
 
         app.updateQuality();
         assertItemEquals(items[0], 10, 12);
+        assertItemEquals(items[1], 10, 50);
 
         app.updateQuality();
         assertItemEquals(items[0], 9, 14);
+        assertItemEquals(items[1], 9, 50);
 
         app.updateQuality(5);
         assertItemEquals(items[0], 4, 25);
+        assertItemEquals(items[1], 4, 50);
 
         app.updateQuality();
         assertItemEquals(items[0], 3, 28);
+        assertItemEquals(items[1], 3, 50);
 
         app.updateQuality(3);
         assertItemEquals(items[0], 0, 37);
+        assertItemEquals(items[1], 0, 50);
 
         app.updateQuality();
         assertItemEquals(items[0], -1, 0);
+        assertItemEquals(items[1], -1, 0);
     }
 }
