e6a28739686c1d08db202f0f631004d5f0f8c843
Will Goring <will@shadowrealm.org>
2019-01-28 22:09:51 +0000

    Tidy-up

--- Java/src/main/java/com/gildedrose/ItemUpdater.java
+++ Java/src/main/java/com/gildedrose/ItemUpdater.java
@@ -76,5 +76,4 @@ public class ItemUpdater {
             item.quality--;
         }
     }
-
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,7 +2,6 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class GildedRoseTest {
