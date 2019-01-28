b2e5566ea40c9e522b5126e85a6e9073cda4f366
Will Goring <will@shadowrealm.org>
2019-01-28 21:23:12 +0000

    Various refactorings for clarity

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -24,47 +24,49 @@ class GildedRose {
             return;
         }
 
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        if (item.name.equals("Aged Brie")
+                || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.sellIn < 11) {
+                                increaseItemQuality(item);
+                            }
+
+                            if (item.sellIn < 6) {
+                                increaseItemQuality(item);
+                            }
                         }
                     }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
+                } else {
+            decreaseItemQuality(item);
         }
 
         item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
+            if (item.name.equals("Aged Brie")) {
+                increaseItemQuality(item);
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.quality = 0;
+                } else {
+                    decreaseItemQuality(item);
                 }
             }
+        }
+    }
+
+    private void increaseItemQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void decreaseItemQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 }
